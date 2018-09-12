a7b91c7c4b22d3046626e1e369634cac02f66f5a
Daniel Stone <Daniel.Stone@tmctrans.com>
2018-09-12 15:44:43 -0500

    routing master

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from 'src/app/shared/event.service'
+import { ToastrService } from '../common/toastr.service'
 
 declare let toastr
 
 @Component({
-    selector: 'events-list',
     template: `
     <div>
       <h1>Upcoming Angular Events</h1>
        <hr/>
        <div class="row">
          <div *ngFor="let event of events" class="col-md-5">
-             <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]
-              ="event"></event-thumbnail>
+             <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
            </div>
        </div>
     </div>
@@ -20,7 +19,8 @@ declare let toastr
 })
 export class EventsListComponent implements OnInit {
     events:any[]
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastr:
+    ToastrService) {
 
   }
 
@@ -29,6 +29,6 @@ export class EventsListComponent implements OnInit {
   }
 
   handleThumbnailClick(eventName) {
-      toastr.success(eventName)
+      this.toastr.success(eventName)
   }
 }
