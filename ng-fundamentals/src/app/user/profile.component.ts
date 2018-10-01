bbc9af3d957db2c58cd1ac00c8a330b400e403ad
Daniel Stone <Daniel.Stone@tmctrans.com>
2018-10-01 14:20:14 -0500

    Created reactive form for login master

--- ng-fundamentals/src/app/user/profile.component.ts
+++ ng-fundamentals/src/app/user/profile.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: '/profile.component.html',
 })
 export class ProfileComponent implements OnInit {
+  profileForm:FormGroup
+
+  constructor(private router:Router, private authService:AuthService) {
+
+  }
 
   ngOnInit() {
-    let firstName = new FormControl()
-    let lastName = new FormControl()
-    
+    let firstName = new FormControl
+    (this.authService.currentUser.firstName)
+    let lastName = new FormControl
+    (this.authService.currentUser.lastName)
+
+    this.profileForm = new FormGroup ({
+      firstName: firstName,
+      lastName: lastName
+    })
   }
-       
+  
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstNAme, formValues.lastName)
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
+  }
+
 }
