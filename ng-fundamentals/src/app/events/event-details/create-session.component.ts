086f362157c991ec67846f36ca16bbcf2c322858
Daniel Stone <Daniel.Stone@tmctrans.com>
2019-03-27 13:28:55 -0500

    set up parent-child communication

--- ng-fundamentals/src/app/events/event-details/create-session.component.ts
+++ ng-fundamentals/src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession, restrictedWords } from '../shared/index'
 
 @Component({
+    selector: 'create-session',
     templateUrl: './create-session.component.html',
 
     styles: [`
@@ -15,6 +16,9 @@ import { ISession, restrictedWords } from '../shared/index'
   `]
 })
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter()
+    @Output() cancelAddSession = new EventEmitter()
+
     newSessionForm: FormGroup
     name: FormControl
     presenter: FormControl
@@ -60,7 +64,12 @@ export class CreateSessionComponent implements OnInit {
             abstract: formValues.abstract,
             voters: []
         }
-        console.log(formValues)
+        this.saveNewSession.emit(session)
+        // console.log(formValues)
         // console.log(session)
+    }
+    
+    cancel() {
+        this.cancelAddSession.emit()
     }
 }
--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core'
 import { EventService } from 'src/app/events/shared/event.service'
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router'
-import { IEvent } from 'src/app/events/shared/index'
+import { IEvent, ISession } from 'src/app/events/shared/index'
 
 @Component({
     templateUrl: './event-details.component.html',
     styles: [`
       .container { padding-left:20px; padding-right: 20px; }
       .event-image: { height: 100px; }
+      a {cursor:pointer}
     `]
 })
 export class EventDetailsComponent {
   event:IEvent
+  addMode:boolean
   constructor(private eventService:EventService,
      private route:ActivatedRoute) {
 
@@ -21,4 +23,19 @@ export class EventDetailsComponent {
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
   }
 
+  addSession() {
+    this.addMode = true
+  }
+
+  saveNewSession(session:ISession) {
+    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
+    session.id = nextId + 1
+    this.event.sessions.push(session)
+    this.eventService.updateEvent(this.event)
+    this.addMode = false
+  }
+
+  cancelAddSession() {
+    this.addMode = false
+  }
 }
